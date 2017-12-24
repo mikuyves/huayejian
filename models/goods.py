8667c27f594b7e73f75cc25536c0dff0d4b5a24b
mikuyves <mikuyves@gmail.com>
2017-12-25 04:25:57 +0800

    Add Sku, Cate, Brand, Supplier, Size and Color models

--- models/goods.py
+++ models/goods.py
@@ -65,7 +65,6 @@ class Prod(leancloud.Object):
 
     @cate.setter
     def cate(self, id):
-        Cate = leancloud.Object.extend('Cate')
         self.set('cate', Cate.create_without_data(id))
 
     @property
@@ -74,7 +73,6 @@ class Prod(leancloud.Object):
 
     @brand.setter
     def brand(self, id):
-        Brand = leancloud.Object.extend('Brand')
         self.set('brand', Brand.create_without_data(id))
 
     @property
@@ -83,6 +81,164 @@ class Prod(leancloud.Object):
 
     @supplier.setter
     def supplier(self, id):
-        Supplier = leancloud.Object.extend('Supplier')
         self.set('supplier', Supplier.create_without_data(id))
 
+    @classmethod
+    def get_skus(cls, id):
+        return Sku.query.equal_to('prod', cls.create_without_data(id)).find()
+
+
+class Sku(leancloud.Object):
+    @property
+    def name(self):
+        return self.get('name')
+
+    @name.setter
+    def name(self, value):
+        self.set('name', value)
+
+    @property
+    def full_name(self):
+        return self.get('full_name')
+
+    @full_name.setter
+    def full_name(self, value):
+        self.set('full_name', value)
+
+    @property
+    def price4(self):
+        return self.get('price4')
+
+    @price4.setter
+    def price4(self, value):
+        self.set('price4', value)
+
+    @property
+    def stock(self):
+        return self.get('stock')
+
+    @stock.setter
+    def stock(self, value):
+        self.set('stock', value)
+
+    @property
+    def size2(self):
+        return self.get('size2')
+
+    @size2.setter
+    def size2(self, value):
+        self.set('size2', value)
+
+    @property
+    def is_sold_out(self):
+        return self.get('isSoldOut')
+
+    @is_sold_out.setter
+    def is_sold_out(self, value):
+        self.set('isSoldOut', value)
+
+    @property
+    def size1(self):
+        return self.get('size1')
+
+    @size1.setter
+    def size1(self, value):
+        self.set('size1', value)
+
+    @property
+    def color(self):
+        return self.get('color')
+
+    @color.setter
+    def color(self, value):
+        self.set('color', value)
+
+    @property
+    def prod(self):
+        return self.get('prod')
+
+    @prod.setter
+    def prod(self, id):
+        self.set('prod', Prod.create_without_data(id))
+
+
+class Cate(leancloud.Object):
+    @property
+    def name(self):
+        return self.get('name')
+
+    @name.setter
+    def name(self, value):
+        self.set('name', value)
+
+
+class Brand(leancloud.Object):
+    @property
+    def name(self):
+        return self.get('name')
+
+    @name.setter
+    def name(self, value):
+        self.set('name', value)
+
+
+class Color(leancloud.Object):
+    @property
+    def name(self):
+        return self.get('name')
+
+    @name.setter
+    def name(self, value):
+        self.set('name', value)
+
+    @property
+    def order(self):
+        return self.get('order')
+
+    @order.setter
+    def order(self, value):
+        self.set('order', value)
+
+
+class Size(leancloud.Object):
+    @property
+    def name(self):
+        return self.get('name')
+
+    @name.setter
+    def name(self, value):
+        self.set('name', value)
+
+    @property
+    def order(self):
+        return self.get('order')
+
+    @order.setter
+    def order(self, value):
+        self.set('order', value)
+
+
+class Supplier(leancloud.Object):
+    @property
+    def name(self):
+        return self.get('name')
+
+    @name.setter
+    def name(self, value):
+        self.set('name', value)
+
+    @property
+    def phone(self):
+        return self.get('phone')
+
+    @phone.setter
+    def phone(self, value):
+        self.set('phone', value)
+
+    @property
+    def wxid(self):
+        return self.get('wxid')
+
+    @wxid.setter
+    def wxid(self, value):
+        self.set('wxid', value)
